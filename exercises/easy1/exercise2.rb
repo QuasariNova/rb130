4f9574633cd28bf7c64b3b8db9c7ab00e585468f
Christopher Perkins <Christopher.Perkins@null.net>
2023-03-19 11:25:41 -0400

    Exercises: Easy I 2 Further Explored

--- exercises/easy1/exercise2.rb
+++ exercises/easy1/exercise2.rb
@@ -3,11 +3,25 @@
 # block is specified, the method should simply return the String 'Does not
 # compute.'.
 
-def compute
+# def compute
+#   return 'Does not compute.' unless block_given?
+#   yield
+# end
+
+# p compute { 5 + 3 } == 8
+# p compute { 'a' + 'b' } == 'ab'
+# p compute == 'Does not compute.'
+
+# Further Exploration:
+# Modify the compute method so it takes a single argument and yields that
+# argument to the block. Provide at least 3 examples of calling this new
+# version of compute, including a no-block call.
+
+def compute(value)
   return 'Does not compute.' unless block_given?
-  yield
+  yield value
 end
 
-p compute { 5 + 3 } == 8
-p compute { 'a' + 'b' } == 'ab'
-p compute == 'Does not compute.'
+p compute(nil) { 5 + 3 } == 8
+p compute('a') { |val| val + 'b' } == 'ab'
+p compute([1]) == 'Does not compute.'
