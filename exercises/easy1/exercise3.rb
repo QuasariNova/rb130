b6e6b71cf943181a21050f7ad11d6debb1d8a481
Christopher Perkins <Christopher.Perkins@null.net>
2023-03-19 11:37:31 -0400

    Exercises: Easy I 3 further explored.

--- exercises/easy1/exercise3.rb
+++ exercises/easy1/exercise3.rb
@@ -2,10 +2,23 @@
 # returns an array that includes all of the missing integers (in order) between
 # the first and last elements of the argument.
 
+# def missing(arr)
+#   arr.first.upto(arr.last).with_object([]) do |int, obj|
+#     obj << int unless arr.include? int
+#   end
+# end
+
+# Further Exploration
+# Can you find other ways to solve this problem? What methods might prove useful?
+
+# I did not use each_cons, which will iterate over overlapping subarrays of the
+# size passed in sequence. It stands for each consecutive sequence. This was
+# the launch school solution.
+
+# Can you find a way to solve this without using a method that requires a block?
+
 def missing(arr)
-  arr.first.upto(arr.last).with_object([]) do |int, obj|
-    obj << int unless arr.include? int
-  end
+  (arr.first..arr.last).to_a - arr
 end
 
 p missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
