ed8c77f1b5c03d1fdbc7a0ced30867ca27fff367
Christopher Perkins <Christopher.Perkins@null.net>
2023-03-21 08:41:39 -0400

    Exercises: Medium Testing accept_money

--- exercises/medium2_testing/cash_register_test.rb
+++ exercises/medium2_testing/cash_register_test.rb
@@ -4,4 +4,18 @@ require_relative 'cash_register'
 require_relative 'transaction'
 
 class CashRegisterTest < MiniTest::Test
+  def setup
+    @register = cash_register.new 100
+    @transaction = transaction.new 10
+  end
+
+  def test_accept_money
+    @transaction.amount_paid = 10
+
+    previous_amount = @register.total_money
+    @register.accept_money @transaction
+    current_amount = @register.total_money
+
+    assert_equal preivous_amount + 10, current_amount
+  end
 end
