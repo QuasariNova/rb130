1bef2efb454567e4795ca70d912f7a032cbbaa7d
Christopher Perkins <Christopher.Perkins@null.net>
2023-03-23 18:05:03 -0400

    Challenges: Easy 6 answered.

--- challenges/easy/challenge6/perfect_numbers.rb
+++ challenges/easy/challenge6/perfect_numbers.rb
@@ -27,3 +27,59 @@
 
 # Write a program that can tell whether a number is perfect, abundant, or
 # deficient.
+
+# rules
+# - PerfectNumber does not make an object, instead it has a class/module method
+#   named classify
+# - classify takes an integer
+# - non positive numbers raise a StandardError
+# - Numbers are 'perfect' if their factors add up to them
+# - Numbers are 'deficient' if their factors add up to a number smaller than
+#   themselves
+# - Numbers are 'abundant' if their factors add up to a number greater than them
+# - Factors does not include themselves
+
+# e
+# 6 => perfect
+# 15 => deficient
+# 24 => abundant
+
+# d
+# Array
+
+# a
+# PerfectNumber::classify
+#   - Given an integer number
+#   - raise an error if integer is not positive
+#   - Find all factors of number save itself
+#     - create empty array named factors
+#     - iterate over numbers starting at 1 and stopping at sqrt of number
+#       - if number is divisible by current number, add both it and its multiple
+#         to factors
+#     - remove duplicate factors and remove number from factors
+#     - return factors
+#   - if sum of factors == number, return 'perfect'
+#   - if sum of factors < number, return 'deficient'
+#   - return 'abundant'
+
+module PerfectNumber
+  def self.classify(number)
+    raise StandardError unless number.positive?
+    sum = find_factors(number).sum
+    return 'perfect' if sum == number
+    return 'deficient' if sum < number
+    'abundant'
+  end
+
+  def self.find_factors(number)
+    factors = []
+
+    1.upto(Integer.sqrt(number)) do |factor|
+      div, mod = number.divmod factor
+      factors += [factor, div].uniq if mod.zero?
+    end
+
+    factors.delete number
+    factors
+  end
+end
