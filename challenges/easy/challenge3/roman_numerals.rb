efc6c5eff0db83fd899e567112e2f91834225957
Christopher Perkins <Christopher.Perkins@null.net>
2023-03-23 15:04:34 -0400

    Challenges: Easy 3 answered.

--- challenges/easy/challenge3/roman_numerals.rb
+++ challenges/easy/challenge3/roman_numerals.rb
@@ -35,3 +35,84 @@
 
 # 2000=MM
 # 8=VIII
+
+# Rules
+# - RomanNumeral object is instantiated using an integer value
+# - RomanNumeral has an instance method to_roman
+# - to_roman converts the integer value to roman numerals
+# - I is numeral for 1, II for 2, etc
+# - V is numeral for 5
+# - X == 10
+# - L == 50
+# - C == 100
+# - D == 500
+# - M == 1000
+# - You can decrement each by 1/10/100 by prepending I/X/C. Though only once
+# = Roman numerals are done with the most significant digit on the left, and
+#   skipping 0s
+
+# e
+# 1990 is MCMXC
+# 2008 is written as MMVIII
+
+# d
+# Integer=>string
+# Hash
+
+# a
+# #initialize
+#   - given an integer, store that integer in @number
+
+# #to_roman
+#   - convert @number to string and set digits to it
+#   - Create an empty string called result
+#   - iterate over each character, maintaining what digit it is looking at.
+#     - If digit is 0, skip
+#     - otherise convert character to roman digit and append to result
+#       - if digit is contained in DIGITs, return that value
+#       - if digit+1 is contained in DIGITs, return those two values
+#       = if digit > 5, return 5 digit + 1s for each remaining point
+#       - return 1s
+#   - return result
+
+class RomanNumeral
+  DIGITS = {1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C', 500 => 'D',
+            1000 => 'M'}
+
+  def initialize(number)
+    @number = number
+  end
+
+  def to_roman
+    digits = @number.to_s.chars
+    total_digits = digits.size
+
+    result = ''
+    digits.each_with_index do |digit, index|
+      next if digit.to_i.zero?
+      result << to_digit(digit.to_i, total_digits - index - 1)
+    end
+
+    result
+  end
+
+  private
+
+  def to_digit(digit, place)
+    power = 10.pow place
+    value = digit * power
+    return DIGITS[value] if DIGITS.include? value # pure
+
+    value += 1 * power # 4 / 9
+    return DIGITS[1 * power] + DIGITS[value] if DIGITS.include? value
+
+    result = ''
+    if digit >= 5 # 5+
+      result << DIGITS[5 * power]
+      digit -= 5
+    end
+
+    digit.times { result << DIGITS[power] } # 1s
+    result
+  end
+end
