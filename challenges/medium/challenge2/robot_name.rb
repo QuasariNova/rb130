e12d43e1ec2b14142cc745bd7e78d5068a291401
Christopher Perkins <Christopher.Perkins@null.net>
2023-03-24 14:04:28 -0400

    Challenges: Medium 2 answered

--- challenges/medium/challenge2/robot_name.rb
+++ challenges/medium/challenge2/robot_name.rb
@@ -10,3 +10,51 @@
 # The names must be random; they should not follow a predictable sequence.
 # Random names means there is a risk of collisions. Your solution should not
 # allow the use of the same name twice.
+
+# rules
+# - Robot class
+#   - has a @name getter
+#   - when instatiated generates a random name of two capital letters and 3
+#     digits
+#     - names must be unique among instances
+#   - has reset method
+#     - generates a new name
+
+# a
+# #name
+#   - return name if @name is not nil
+#   - Otherwise generate random name until @@used_names does not include the
+#     name
+#   - add name to @@used_names
+#   - return name
+
+# #reset
+#   - Set @name to nil
+
+
+class Robot
+  LETTERS = ('A'..'Z').to_a
+  NUMBERS = (1..9).to_a
+  @@used_names = []
+
+  def name
+    if @name.nil?
+      @name = generate_name while @name.nil? || @@used_names.include?(@name)
+      @@used_names << @name
+    end
+    @name
+  end
+
+  def reset
+    @@used_names.delete @name
+    @name = nil
+  end
+
+  private
+
+  def generate_name
+    name = 2.times.with_object([]) { |_,obj| obj << LETTERS.sample }
+    name += 3.times.with_object([]) { |_,obj| obj << NUMBERS.sample }
+    name.join
+  end
+end
