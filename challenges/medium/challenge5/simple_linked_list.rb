ff05089bf04a4d40983fd337f659b9ca97430bd4
Christopher Perkins <Christopher.Perkins@null.net>
2023-03-25 07:34:57 -0400

    Challenges: Medium 5 Element implemented.

--- challenges/medium/challenge5/simple_linked_list.rb
+++ challenges/medium/challenge5/simple_linked_list.rb
@@ -10,3 +10,55 @@
 # Let's create a singly linked list whose elements may contain a range of data
 # such as the numbers 1-10. Provide methods to reverse the linked list and
 # convert a linked list to and from an array.
+
+# rules
+# - Element class
+#   - ::new has 2 arguments
+#     - First is integer
+#     - Second is another Element object, default to nil
+#   - #datum
+#     - returns value
+#   - #tail?
+#     - returns whther the element passed during instantiation is nil
+#   - #next
+#     - returns the element passed during instantiation.
+
+# - SimpleLinkedList class
+#   - ::new has 0 arguments
+#   - ::from_a
+#     - converts an array to SimpleLinkedList
+#   - #size
+#     - returns size of list
+#   - #push takes 1 argument
+#     - adds value to list as new Element
+#     - newest element is linked to the last element added
+#   - #pop
+#     - returns datum of last element
+#   - #head
+#     - returns last Element added
+#   - #peek
+#     - returns datum from last Element
+#   - #to_a
+#     - Converts SimpleLinkedList to array
+#   - #reverse
+#     - Reverses SimpleLinkedList maybe returning a new object
+
+# d
+# Array
+
+class Element
+  attr_reader :datum
+
+  def initialize(datum, link = nil)
+    @datum = datum
+    @link = link
+  end
+
+  def tail?
+    @link.nil?
+  end
+
+  def next
+    @link
+  end
+end
--- challenges/medium/challenge5/simple_linked_list_test.rb
+++ challenges/medium/challenge5/simple_linked_list_test.rb
@@ -9,19 +9,16 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_element_tail
-    skip
     element = Element.new(1)
     assert element.tail?
   end
 
   def test_element_next_default
-    skip
     element = Element.new(1)
     assert_nil element.next
   end
 
   def test_element_next_initialization
-    skip
     element1 = Element.new(1)
     element2 = Element.new(2, element1)
     assert_equal element1, element2.next
